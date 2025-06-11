9c607bcac6b2ab4cea4e57263bb0f4d72eb6cf5f
aleksanderZak7 <olekzak7@gmail.com>
2025-06-11 21:15:04 +0200

    Use CrossValidation in main.py

--- source/main.py
+++ source/main.py
@@ -1,13 +1,16 @@
 import time
+import numpy as np
 
 import Config
 from utils import my_print
+from cross_val import CrossValidation
 from train_test import train_model, test_model
 
 
 if __name__ == '__main__':
     epoches: int = 2
     train: bool = True
+    cross_val: bool = True
 
     conf = Config.config()
     conf.train = train
@@ -20,13 +23,21 @@ if __name__ == '__main__':
         my_print('valid_data_path:  {}'.format(conf.valid_data_path))
         my_print('epoches num:      {}'.format(conf.epoches))
 
-        my_activation = train_model(conf)
-        my_print('Total epoches ({})'.format(conf.epoches))
-        my_activation.training()
+        if cross_val:
+            scores = CrossValidation(cv=2, conf=conf)
+            scores.run()
+            
+            my_print(scores)
+            my_print(f"DICE mean: {np.mean(scores.DICE_):.4f}\nDICE std: {np.std(scores.DICE_):.4f}")
+        else:
+            my_activation = train_model(conf)
+            my_print('Total epoches ({})'.format(conf.epoches))
+            my_activation.training()
         end = time.time()
     else:
         my_print('test_data_path: {}'.format(conf.test_data_path))
         my_activation = test_model(conf)
         my_activation.test()
         end = time.time()
+        
     my_print('Running time:{}'.format(str(end-start)))
