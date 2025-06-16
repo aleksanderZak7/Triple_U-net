274aacba8f754d85f643460a025feac1aab95bf3
aleksanderZak7 <olekzak7@gmail.com>
2025-06-16 11:38:00 +0200

    Refactor data.py in preparation for transpose modification

--- source/data.py
+++ source/data.py
@@ -18,22 +18,23 @@ class trainGenerator(Dataset):
         self.img_ids    =   sorted(os.listdir(self.data_dir))
         self.error_name =   True
         self.right_name =   False
-    def just_img_name(self,img_id):
-        if img_id.find('pre')>=0 or  img_id.find('sep')>=0 or  img_id.find('mask')>=0:
+        
+    def just_img_name(self, img_id):
+        if img_id.find('pre') >= 0 or img_id.find('sep') >= 0 or img_id.find('mask') >= 0:
             return self.error_name
         return self.right_name    
         
-    def load_image(self, index,norm=True,no_transpose=False):
+    def load_image(self, index, norm=True, no_transpose=False):
         img_id  = self.img_ids[index]
         if self.just_img_name(img_id):
-            return torch.FloatTensor([]),img_id
+            return torch.FloatTensor([]), img_id
         imgFile = os.path.join(self.data_dir, img_id)
         img     = skimage.io.imread(imgFile)
         if no_transpose:
-            return img,img_id
+            return img, img_id
         if norm:
-            return torch.FloatTensor(np.transpose(img, (2,0,1))/255),img_id
-        return torch.FloatTensor(np.transpose(img, (2,0,1))),img_id
+            return torch.FloatTensor(np.transpose(img, (2,0,1)) / 255), img_id
+        return torch.FloatTensor(np.transpose(img, (2,0,1))), img_id
         
     def load_mask(self, index,no_tensor=False):
         mask_ids    = self.img_ids[index]
@@ -42,10 +43,10 @@ class trainGenerator(Dataset):
 
         if no_tensor:
             return mask
-        return torch.FloatTensor(mask>0)
+        return torch.FloatTensor(mask > 0)
 
         
-    def load_edge(self, index,no_tensor=False):
+    def load_edge(self, index, no_tensor=False):
         feat_ids    = self.img_ids[index]
         
         imgFile     = os.path.join(self.edge_dir, feat_ids)
@@ -55,36 +56,33 @@ class trainGenerator(Dataset):
             return mask
         return torch.FloatTensor(mask>0)
         
-    def load_HE(self, index,norm=True,no_transpose=False):
+    def load_HE(self, index, no_transpose=False):
         img_id  = self.img_ids[index]
         if self.just_img_name(img_id):
             return torch.FloatTensor([])
+        
         imgFile = os.path.join(self.data_dir, img_id)
         img     = skimage.io.imread(imgFile)
         
-        HE      = separate_stain(img)
         HE = separate_stain(img)
-        
-        #HE      = skimage.color.gray2rgb(HE)
-        #print(f"data.py 71 -> HE shape: {HE.shape}") # data.py 71 -> HE shape: (64, 64, 3, 3) !!!!!!!!!!!
+        #HE      = skimage.color.gray2rgb(HE) # -> HE shape: (64, 64, 3, 3) !!!!!!!!!!!
         
         if no_transpose:
             return HE
-        if norm:
-            output = torch.FloatTensor(np.transpose(HE, (2,0,1))/255)
-            return output
-        output = torch.FloatTensor(np.transpose(HE, (2,0,1))/255)
+
+        output = torch.FloatTensor(np.transpose(HE, (2, 0, 1))/255)
         return output
         
     def __getitem__(self, item):
-        img,file     = self.load_image(item,no_transpose=True)
-        masks    = self.load_mask(item,no_tensor=True)
-        edge    = self.load_edge(item,no_tensor=True)
-        HE      = self.load_HE(item,no_transpose=True)
+        img, _     = self.load_image(item, no_transpose=True)
+        masks    = self.load_mask(item, no_tensor=True)
+        edge    = self.load_edge(item, no_tensor=True)
+        HE      = self.load_HE(item, no_transpose=True)
 
         if self.transform is not None:
-            return self.transform(img, masks,HE,edge)
-        return img,HE,masks,edge
+            return self.transform(img, masks, HE, edge)
+        
+        return img, HE, masks, edge
         
     def __len__(self):
         return len(self.img_ids)
@@ -96,11 +94,10 @@ def save_output(save_path,img,name):
         cv2.imwrite(imgFile,np.array(img[i,:,:].cpu().detach().numpy())*255)
         
 def collater(data):
+    HE = []
     img = []
-    masks = []
-    HE=[]
     edge = []
-    file=[]
+    masks = []
 
     for sample in data:
         img.append(sample[0])
@@ -108,18 +105,18 @@ def collater(data):
         edge.append(sample[3])
         masks.append(sample[2])
 
-    HE = torch.stack(HE,0)
-    img = torch.stack(img,0)
-    masks = torch.stack(masks,0)
-    edge = torch.stack(edge,0)
+    HE = torch.stack(HE, 0)
+    img = torch.stack(img, 0)
+    masks = torch.stack(masks, 0)
+    edge = torch.stack(edge, 0)
 
-    return img,HE,masks,edge
+    return img, HE, masks, edge
 
-def seg_loss(prediction,ground_truth):
+def seg_loss(prediction, ground_truth):
     ground_truth = torch.squeeze(ground_truth.cpu()).view(-1) 
     prediction = torch.squeeze(prediction.cpu()).view(-1)
 
-    loss = nn.BCELoss()(prediction,ground_truth)
+    loss = nn.BCELoss()(prediction, ground_truth)
     return loss
 
 def soft_dice(input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
@@ -139,7 +136,7 @@ def soft_dice(input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     return loss
     
 
-def soft_truncate_ce_loss(pre,label,  delta=0.2):
+def soft_truncate_ce_loss(pre,label, delta=0.2):
     smooth = 1e-5
     pre = torch.squeeze(pre.cpu())
     label = torch.squeeze(label.cpu())
@@ -151,8 +148,7 @@ def soft_truncate_ce_loss(pre,label,  delta=0.2):
     condition = torch.le(ret,np.log(delta))
     return torch.mean(torch.where(condition, smooth_truncate,-ret))
     
-def IOU_loss(pre,label,batch_size, delta=0.5):
-    smooth = 1e-5
+def IOU_loss(pre,label, batch_size):
     dim=(1, 2)
     pre = torch.squeeze(pre.cpu())
     label = torch.squeeze(label.cpu())
@@ -161,7 +157,7 @@ def IOU_loss(pre,label,batch_size, delta=0.5):
     IoU = torch.sum(intersection,dim)/torch.sum(union,dim)
     return (1-torch.mean(IoU))/batch_size
 
-def FP_loss(pre,label):
+def FP_loss(pre, label):
     smooth = 1e-5
     pre = torch.squeeze(pre.cpu())
     label = torch.squeeze(label.cpu())
@@ -186,6 +182,6 @@ class LossVariance(nn.Module):
                 instance = input[k][:, target[k] == val]
                 if instance.size(1) > 1:
                     sum_var = sum_var + torch.sum(torch.var(instance))
-            loss = loss+ sum_var / (len(unique_vals) + 1e-8)
-        loss = loss/ B
+            loss = loss + sum_var / (len(unique_vals) + 1e-8)
+        loss = loss / B
         return loss.cpu()
